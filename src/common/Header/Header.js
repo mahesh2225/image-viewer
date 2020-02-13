ea77242c734e6a216ff596df4088ac82539f47dc
mahesh2225 <g.mahendra22@gmail.com>
2020-02-13 21:08:03 +0530

    create header title

---
+++ src/common/Header/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+
+class Header extends Component {
+    constructor() {
+        super();
+      
+
+    }
+   
+    render() {
+        return (
+            <div className="header">
+                <div className="title">Image Viewer</div>
+                
+                <div className="header-right">
+                   
+                  
+                </div> 
+            </div>
+
+        )
+    }
+}
+
+export default Header;
